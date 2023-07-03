3baed7e3d039222f6a5d52defb0f9e67ff9d5844
Giorgio Ventura <gventuraagramonte@gmail.com>
2023-07-03 17:24:44 -0500

    trabajando con incremental static generation

--- pages/pokemon/[id].tsx
+++ pages/pokemon/[id].tsx
@@ -79,7 +79,7 @@ export const getStaticPaths: GetStaticPaths = () => {
         paths: pokemon151.map(id => ({
             params: { id }
         })),
-        fallback: false
+        fallback: 'blocking'
     }
 }
 
@@ -87,11 +87,22 @@ export const getStaticProps: GetStaticProps = async ({ params }) => {
 
     const { id } = params as { id: string }
 
+    const pokemon = await getPokemonInfo(id);
+
+    if (!pokemon) {
+        return {
+            redirect: {
+                destination: '/',
+                permanent: false
+            }
+        }
+    }
 
     return {
         props: {
-            pokemon: await getPokemonInfo(id)
-        }
+            pokemon
+        },
+        revalidate: 86400,
     }
 }
 
